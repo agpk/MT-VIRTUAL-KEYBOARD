92c85c5056a9fbbb1d09401d7613c634bc86f186
agpk <109487033+agpk@users.noreply.github.com>
2022-07-21 03:26:03 +0200

    Update mtvk.cpp

--- mt/mtvk.cpp
+++ mt/mtvk.cpp
@@ -1,7 +1,16 @@
-//copyright(c) agpk - 2022
-//under the GPLv3 license
+
+//               MTVK Library
+//     Copyright (c) Alan Kubiak, 2022.
+//
+//  This project is under the GPLv3 license.
+//           For the LLEC project.
+//
+//   --------------------------------------
+//        Mt(c)Virtual Keyboard (MTVK)
+//   --------------------------------------
+
 #include <M5StickC.h>
-#define MT_KEYS "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890.:;?!/\\_-()[]% <"
+#define MT_KEYS "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890+-*/%=()<>#@\". <"
 #define TYPEC 500
 #define MXLN 0xFF
 #define TRM "-- "
@@ -11,14 +20,13 @@
 char cc[MXLN];
 
 void OPEN_MT(char* dest, char* TEXT) {
-  int initRot = M5.Lcd.getRotation();
-  int initCX = M5.Lcd.getCursorX();
-  int initCY = M5.Lcd.getCursorY();
+  int initRot=M5.Lcd.getRotation();
+  int initCX=M5.Lcd.getCursorX();
+  int initCY=M5.Lcd.getCursorY();
   int c=0,stck=0,m=1,e=0;
   M5.Lcd.setTextSize(1);
   M5.Lcd.setRotation(3);
   while(true) {
-    char currChar = MT_KEYS[c];
     M5.Lcd.fillScreen(BLACK);
     M5.Lcd.setCursor(0,0);
     M5.Lcd.printf("Mt(c)virtual keyboard v%s\n%s",VER,TEXT);
@@ -59,7 +67,7 @@ void OPEN_MT(char* dest, char* TEXT) {
           }
         }
         else {
-          cc[stck]=currChar;
+          cc[stck]=MT_KEYS[c];
           stck++;m=0;
           delay(TYPEC);break;
         }
@@ -69,7 +77,7 @@ void OPEN_MT(char* dest, char* TEXT) {
         M5.Lcd.setRotation(initRot);
         M5.Lcd.setCursor(initCX,initCY);
         strcpy(dest,cc); // not secure
-        memset(cc, 0, sizeof(cc));
+        memset(cc,0,sizeof(cc));
         delay(TYPEC);
         return;
       }
