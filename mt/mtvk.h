d83f639902e0bb69b22a1e50c63d314e03b2cd64
agpk <109487033+agpk@users.noreply.github.com>
2022-07-21 03:27:12 +0200

    Update mtvk.h

--- mt/mtvk.h
+++ mt/mtvk.h
@@ -1,3 +1,14 @@
+
+//               MTVK Library
+//     Copyright (c) Alan Kubiak, 2022.
+//
+//  This project is under the GPLv3 license.
+//           For the LLEC project.
+//
+//   --------------------------------------
+//        Mt(c)Virtual Keyboard (MTVK)
+//   --------------------------------------
+
 #include "mtvk.cpp"
 #ifndef MTVK_H
 #define MTVK_H
